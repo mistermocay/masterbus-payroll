899fd9b4b571e1fb00f2ef4566dda48c639e7603
mistermocay <fahralathallahchandra24@gmail.com>
2026-07-14 16:58:26 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,38 +1,30 @@
-console.log("Firebase Loaded");
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-
 import {
-    getFirestore,
-    collection,
-    addDoc
+  getFirestore,
+  collection,
+  addDoc
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCy5Cg24RpnfXRYnMIC8s_p4AgDGK0M55Y",
-    authDomain: "master-bus-payroll.firebaseapp.com",
-    projectId: "master-bus-payroll",
-    storageBucket: "master-bus-payroll.firebasestorage.app",
-    messagingSenderId: "1093538073835",
-    appId: "1:1093538073835:web:1e8f8f3001a15e75e042ae",
-    measurementId: "G-Q34GP1R77T"
+  apiKey: "AIzaSyCy5Cg24RpnfXRYnMIC8s_p4AgDGK0M55Y",
+  authDomain: "master-bus-payroll.firebaseapp.com",
+  projectId: "master-bus-payroll",
+  storageBucket: "master-bus-payroll.firebasestorage.app",
+  messagingSenderId: "1093538073835",
+  appId: "1:1093538073835:web:1e8f8f3001a15e75e042ae",
+  measurementId: "G-Q34GP1R77T"
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
-// TEST
-window.testFirebase = async function () {
+console.log("Firebase berhasil dimuat!");
 
-    await addDoc(collection(db, "employees"), {
+window.testFirebase = async () => {
+  await addDoc(collection(db, "employees"), {
+    nama: "Test",
+    bagian: "ADMIN"
+  });
 
-        nama: "Test Firebase",
-        bagian: "ADMIN",
-        gaji: 4000000,
-        waktu: new Date()
-
-    });
-
-    alert("Data berhasil dikirim ke Firebase!");
-
-}
+  alert("Berhasil!");
+};
