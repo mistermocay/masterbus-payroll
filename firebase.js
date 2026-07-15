4db869ff37fb9dbd039a97e86143cf93b1f7152d
mistermocay <fahralathallahchandra24@gmail.com>
2026-07-15 10:46:00 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,29 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-
-import {
-    getFirestore
-} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
-
-    apiKey: "...",
-
-    authDomain: "...",
-
-    projectId: "...",
-
-    storageBucket: "...",
-
-    messagingSenderId: "...",
-
-    appId: "...",
-
-    measurementId: "..."
-
+    apiKey: "AIzaSyCy5Cg24RpnfXRYnMIC8s_p4AgDGK0M55Y",
+    authDomain: "master-bus-payroll.firebaseapp.com",
+    projectId: "master-bus-payroll",
+    storageBucket: "master-bus-payroll.firebasestorage.app",
+    messagingSenderId: "1093538073835",
+    appId: "1:1093538073835:web:1e8f8f3001a15e75e042ae",
+    measurementId: "G-Q34GP1R77T"
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
 window.db = db;
